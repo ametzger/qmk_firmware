1f63fc5917bf62464d5b4360dad0e5c7e04577bb
Alex Metzger <asm@asm.io>
2021-04-12 11:42:28 -0400

    add :coffee:

--- keyboards/nk65/keymaps/ametzger/keymap.c
+++ keyboards/nk65/keymaps/ametzger/keymap.c
@@ -15,13 +15,17 @@
  */
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    THUMBSUP = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_65_ansi( /* Base */
     KC_GESC, KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,    \
-    KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,   \
+    KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, THUMBSUP, \
     KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,   \
     KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END,    \
-    KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC,                          KC_RGUI, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
+    KC_LCTL, KC_LALT, KC_LCMD,                KC_SPC,                          KC_RCMD, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
 
 [1] = LAYOUT_65_ansi( /* FN */
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_TRNS,\
@@ -45,6 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS,                KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
+
 void matrix_init_user(void) {
   //user initialization
 }
@@ -53,6 +58,15 @@ void matrix_scan_user(void) {
   //user matrix
 }
 
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
+    switch (keycode) {
+        case THUMBSUP:
+            if (record->event.pressed) {
+                SEND_STRING(":thumbsup:");
+            }
+            break;
+    }
+
+    return true;
+};
