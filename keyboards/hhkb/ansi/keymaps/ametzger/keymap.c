a8ac10f7e3487bf8149ec95dbcff9ba373751bb8
Alex Metzger <asm@asm.io>
2024-12-10 13:23:20 -0500

    holy shit hhkb with superpowers, awesome

--- keyboards/hhkb/ansi/keymaps/ametzger/keymap.c
+++ keyboards/hhkb/ansi/keymaps/ametzger/keymap.c
@@ -6,6 +6,13 @@
 #define BASE 0
 #define HHKB 1
 
+enum custom_keycodes {
+    ASMIO = SAFE_RANGE,
+    JF_EMAIL,
+    ASM_EMAIL,
+    GMAIL_EMAIL,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* BASE Level: Default Layer
@@ -35,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       | Pwr  | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   | Ins   | Del |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |     |     |     |    |    |    |    | Psc | Slk | Pus | Up  |       | Backs |     |
+      | ASM_EMAIL | JF_EMAIL | ASMIO |     |    |    |    |    | Psc | Slk | Pus | Up  |       | Backs |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       |      | VoD | VoU | Mut |    |    | *  | /  | Hom | PgU | Lef | Rig | Enter |       |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
@@ -49,8 +56,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [HHKB] = LAYOUT(
-        KC_PWR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   KC_TRNS, KC_BSPC,
-        KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_PENT,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
+        KC_TRNS,   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
+        ASM_EMAIL, JF_EMAIL,  ASMIO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   KC_TRNS, KC_BSPC,
+        KC_TRNS,   KC_VOLD,   KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_PENT,
+        KC_TRNS,   MEH(KC_5), KC_MCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS,
+        KC_TRNS,   KC_TRNS,                             KC_MPLY,                   KC_TRNS, KC_TRNS)};
+
+void matrix_init_user(void) {
+  //user initialization
+}
+
+void matrix_scan_user(void) {
+  //user matrix
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case ASMIO:
+    if (record->event.pressed) {
+      SEND_STRING("https://asm.io/i/");
+    }
+    break;
+  case JF_EMAIL:
+    if (record->event.pressed) {
+      SEND_STRING("asm@jellyfish.co");
+    }
+    break;
+  case ASM_EMAIL:
+    if (record->event.pressed) {
+      SEND_STRING("asm@asm.io");
+    }
+    break;
+  case GMAIL_EMAIL:
+    if (record->event.pressed) {
+      SEND_STRING("alex.metzger@gmail.com");
+    }
+    break;
+  }
+
+  return true;
+};
