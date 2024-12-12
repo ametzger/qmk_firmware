f8dabadd1b1df94b84de101dcebd72323315c846
Alex Metzger <asm@asm.io>
2024-12-12 12:26:11 -0500

    hhkb: better modifiers

--- keyboards/hhkb/ansi/keymaps/ametzger/keymap.c
+++ keyboards/hhkb/ansi/keymaps/ametzger/keymap.c
@@ -26,9 +26,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      | Shift | Z | X | C | V | B | N | M | , | . | / | Shift | Fn0 |       |   |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
 
-            |------+------+-----------------------+------+------|
-            | LAlt | LGUI | ******* Space ******* | RGUI | RAlt |
-            |------+------+-----------------------+------+------|
+              |------+------+-----------------------+-----+-------|
+              | LAlt | LGUI | ******* Space ******* | Meh | Hyper |
+              |------+------+-----------------------+-----+-------|
+
+      Meh - C-S-M (control, shift, meta)
+      Hyper = C-S-M-s (control, shift, meta, super)
     */
 
     [BASE] = LAYOUT( //  default layer
@@ -36,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O, KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(HHKB),
-        KC_LALT, KC_LGUI, /*        */ KC_SPC, KC_RGUI, KC_RALT),
+        KC_LALT, KC_LGUI, /*        */ KC_SPC, KC_MEH, KC_HYPR),
 
     /* Layer HHKB: HHKB mode (HHKB Fn)
       |-----------+----------+-------+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
@@ -54,6 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  |------+------+----------------------+------+------+
 
      */
+
+    /* NOTE(asm,2024-12-12): DON'T OVERRIDE MODIFIERS IN A LAYER! YOU WILL REGRET IT! */
 
     [HHKB] = LAYOUT(
         KC_TRNS,   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
